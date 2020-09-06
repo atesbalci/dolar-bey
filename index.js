dbb727436c8d38d03646704880b838b336c45c6b
Ates Balci <atesbalci@gmail.com>
2020-09-06 10:51:46 +0300

    Switched to express impl

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 // const { startDolarBot } = require("./bot");
-const { startServer } = require("./telegram_bot");
+const { prepareDolarTelegramExpress } = require("./telegram_bot");
+const express = require('express');
+const app = express();
+
+function startTelegramServer() {
+  app.use(express.json());
+  prepareDolarTelegramExpress(app);
+  app.listen(process.env.PORT || 80, () => console.log('Listening...'));
+}
 
 // startDolarBot(process.env.BOT_TOKEN);
-startServer();
+startTelegramServer();
--- telegram_bot.js
+++ telegram_bot.js
@@ -1,34 +1,23 @@
 const https = require('https');
-const express = require('express');
 const { refreshDolar, DolarData } = require('./dolar_utils');
-const app = express();
 
-function handler(request, response) {
-  response.send('All done!');
-  
-  // try {
-  //   let requestParsed = JSON.parse(request.body);
-  //   let chatId = requestParsed.message.chat.id;
-  //   console.log(chatId);
-  //   if (request.url === '/dolarkac') {
-  //     refreshDolar(dolarData => {
-  //       console.log(dolarData);
-  //       sendMessage(chatId, `Durumum:+${dolarData.current}`);
-  //     }, null);
-  //   }
-  // } catch (error) {
-  //   console.log(error);
-  // }
+module.exports.prepareDolarTelegramExpress = function prepareDolarTelegramExpress(expressApp) {  
+  expressApp.post('/getdolar', (req, res) => {
+    res.send('All done!');
+    try {
+      console.log(req.body);
+      let chatId = req.body.message.chat.id;
+      console.log(chatId);
+      refreshDolar(dolarData => {
+        console.log(dolarData);
+        sendMessage(chatId, `Durumum:+${dolarData.current}`);
+      }, null);
+    } catch (error) {
+      console.log(error);
+    }
+  });
 }
 
 function sendMessage(chatId, message) {
   https.get(`${process.env.DOLAR_TELEGRAM_GROUP_URL_PREFIX}${chatId}&text=${message}`);
-}
-
-module.exports.startServer = function startServer() {
-  app.get('/', handler);
-  let port = process.env.PORT || 80;
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-  });
 }
