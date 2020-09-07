637beeac468cb2998113238a2e62beb59de4b764
Ates Balci <atesbalci@gmail.com>
2020-09-08 01:35:57 +0300

    Modular handler

--- telegram_bot.js
+++ telegram_bot.js
@@ -1,33 +1,33 @@
 const https = require('https');
 const { refreshDolar, DolarData, recordListeners } = require('./dolar_utils');
 
-module.exports.prepareDolarTelegramExpress = function prepareDolarTelegramExpress(expressApp) {  
-  recordListeners.push(onRecord);
-  expressApp.post('/', (req, res) => {
-    res.send('All done!');
-    try {
-      let chatId = req.body.message.chat.id;
-      let command = req.body.message.text;
+module.exports.handleDolarTelegram = function handleDolarTelegram(req, res) {
+  try {
+    let chatId = req.body.message.chat.id;
+    let command = req.body.message.text;
 
-      console.log(chatId);
-      if (command.includes('/dolarkac')) {
-        refreshDolar(dolarData => {
-          sendMessage(chatId, `Durumum:+${dolarData.current}`);
-        });
-      } else if (command.includes('/rekorkac')) {
-        refreshDolar(dolarData => {
-          sendMessage(chatId, `Rekorum:+${dolarData.record}`);
-        });
+    console.log(chatId);
+    if (command.includes('/dolarkac')) {
+      refreshDolar(dolarData => {
+        sendMessage(chatId, `Durumum:+${dolarData.current}`);
+      });
+    } else if (command.includes('/rekorkac')) {
+      refreshDolar(dolarData => {
+        sendMessage(chatId, `Rekorum:+${dolarData.record}`);
+      });
 
-      } else if (command.includes('/gunlukrekor')) {
-        refreshDolar(dolarData => {
-          sendMessage(chatId, `Gunluk+Rekorum:+${dolarData.dailyRecord}`);
-        });
-      }
-    } catch (error) {
-      console.log(error);
+    } else if (command.includes('/gunlukrekor')) {
+      refreshDolar(dolarData => {
+        sendMessage(chatId, `Gunluk+Rekorum:+${dolarData.dailyRecord}`);
+      });
     }
-  });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports.initDolarTelegram = function initDolarTelegram() {
+  recordListeners.push(onRecord);
 }
 
 function sendMessage(chatId, message) {
