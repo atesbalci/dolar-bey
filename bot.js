7f3c71e12712f5b88914c783d381ecc397aed96f
Ates Balci <atesbalci@gmail.com>
2021-10-22 01:49:09 +0300

    Implemented discord subscriptions

--- bot.js
+++ bot.js
@@ -1,19 +1,19 @@
 const { Client, Intents } = require('discord.js');
 const { getTime } = require('./utils');
-const { refreshDolar, recordListeners } = require('./dolar_utils');
+const { refreshDolar, recordListeners, localReferenceListeners } = require('./dolar_utils');
+const { getSubsFor, Platform, SubscriptionType, subExists, addSub, removeSub } = require('./config');
 
 const dolarKacCommandName = 'dolarkac';
 const rekorKacCommandName = 'rekorkac';
 const gunlukRekorKacCommandName = 'gunlukrekorkac';
+const subStatusCommandName = 'substatus';
+const subscribeCommandName = 'subscribe';
 
-let channel;
-
-function onRecord(dolarData) {
-  channel.send(`REKORLARDAYIM: ${dolarData.record}`);
-}
+let client;
 
 async function handleInteraction(interaction) {
   if (interaction.isCommand()) {
+    const chatId = interaction.channelId;
     const commandName = interaction.commandName;
     await interaction.deferReply();
     const dolarData = await refreshDolar();
@@ -23,12 +23,26 @@ async function handleInteraction(interaction) {
       interaction.followUp(`Rekorum: ${dolarData.record}`);
     } else if (commandName === dolarKacCommandName) {
       interaction.followUp(`Durumum: ${dolarData.current}`);
+    } else if (commandName === subStatusCommandName) {
+      let msg = `Rekor Mesajlari: ${subExists(Platform.DISCORD, SubscriptionType.RECORD, chatId) ? 'ACIK' : 'KAPALI'}\n`;
+      msg += `Cikis/Inis Mesajlari: ${subExists(Platform.DISCORD, SubscriptionType.RISE_AND_FALL, chatId) ? 'ACIK' : 'KAPALI'}`;
+      interaction.followUp(msg);
+    } else if (commandName === subscribeCommandName) {
+      const subType = interaction.options.getInteger('category');
+      if (!subExists(Platform.DISCORD, subType, chatId)) {
+        addSub(Platform.DISCORD, subType, chatId);
+        interaction.followUp('Yazacam buraya...');
+      }
+      else {
+        removeSub(Platform.DISCORD, subType, chatId);
+        interaction.followUp('Tamam tamam... sustum...');
+      }
     }
   }
 }
 
 module.exports.startDolarBot = function startDolarBot(token) {
-  const client = new Client({ 
+  client = new Client({ 
     intents: [
       Intents.FLAGS.GUILDS,
       Intents.FLAGS.GUILD_MESSAGES,
@@ -55,6 +69,33 @@ module.exports.startDolarBot = function startDolarBot(token) {
         description: 'Dolarda gunluk rekor kac?',
         options: []
       },
+      {
+        name: subscribeCommandName,
+        description: 'Mesaj abonelikleri',
+        options: [
+          {
+            type: 'INTEGER',
+            name: 'category',
+            description: 'Abonelik kategorisi',
+            required: true,
+            choices: [
+              {
+                name: 'Rekor',
+                value: SubscriptionType.RECORD
+              },
+              {
+                name: 'Inis ve cikis',
+                value: SubscriptionType.RISE_AND_FALL
+              }
+            ]
+          }
+        ]
+      },
+      {
+        name: subStatusCommandName,
+        description: 'Bu kanalin mesaj aboneliklerini listele',
+        options: []
+      }
     ]);
   });
 
@@ -66,7 +107,33 @@ module.exports.startDolarBot = function startDolarBot(token) {
 
   refreshDolar();
   recordListeners.push(onRecord);
+  localReferenceListeners.push(onLocalReferenceChange);
   setInterval(() => refreshDolar(), 300000);
 
   client.login(token);
 }
+
+function onRecord(dolarData) {
+  sendMessageToGroups(`REKORLARDAYIM: ${dolarData.record}`, SubscriptionType.RECORD);
+}
+
+function onLocalReferenceChange(dolarData, diff) {
+  let message;
+  if (diff > 0) {
+    message = `CIKI$LARDAYIM: ${dolarData.current}`;
+  } else {
+    message = `DU$U$LERDEYIM: ${dolarData.current}`;
+  }
+  
+  sendMessageToGroups(message, SubscriptionType.RISE_AND_FALL);
+}
+
+function sendMessageToGroups(message, subType) {
+  getSubsFor(Platform.DISCORD, subType).forEach(sub => {
+    client.channels.fetch(sub.chatId).then(channel => channel.send(message));
+  });
+}
+
+// setInterval(() => {
+//   sendMessageToGroups("Test record", SubscriptionType.RECORD);
+// }, 5000);
--- runner_template.sh
+++ runner_template.sh
@@ -1,8 +1,6 @@
 #Clone and rename this to runner.sh
 
 BOT_TOKEN='bot token' \
-RECORD_CHANNEL_ID='record channel id' \
-DOLAR_TELEGRAM_GROUP_ID='group id' \
 DOLAR_RECORD_TOLERANCE='0.002' \
 DOLAR_LOCAL_TOLERANCE='0.05' \
 TELEGRAM_BOT_TOKEN='telegram bot token' \
